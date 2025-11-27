d0fd3505ac06f0ca6d2f053555512fc161a7b5a4
Jonas <jonasakenande7@gmail.com>
2025-11-27 09:32:45 +0100

    commit

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require ('express');
+const bodyParser = require ('body-parser');
+const mysql = require ('mysql');
+const cors = require ('cors');
+const app = express ();
+const port = 8080;
