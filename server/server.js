bc0fc8bd0dfb8e97e29cfac1484c00b1a452ede4
kaaris <kaaris@gmail.com>
2025-11-27 10:54:36 +0100

    replace server.js

--- server/server.js
+++
@@ -1,6 +0,0 @@
-const express = require ('express');
-const bodyParser = require ('body-parser');
-const mysql = require ('mysql');
-const cors = require ('cors');
-const app = express ();
-const port = 8080;
